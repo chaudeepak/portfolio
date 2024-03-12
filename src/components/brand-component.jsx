67c2261443788c1655f8996a15c18a18023f2b78
Deepak Chaudhary <chaudhary.deepak0124@gmail.com>
2024-03-12 15:16:12 +0545

    brand change

--- src/components/brand-component.jsx
+++ src/components/brand-component.jsx
@@ -1,11 +1,13 @@
-import { Container } from "react-bootstrap";
+import { Container, Image } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
+import DC from "../assets/images/dc.svg"
 // import "../css/style.css";
 
 const BrandComponent = () => {
     return(<>
         <Container>
-            <NavLink to="/" style={{textDecoration: 'none'}}><h3>Deepak Chaudhary</h3></NavLink>
+            {/* <NavLink to="/" style={{textDecoration: 'none'}}><h3>Deepak Chaudhary</h3></NavLink> */}
+            <NavLink to="/" style={{textDecoration: 'none'}}><Image src={DC} style={{height: '70px'}}></Image></NavLink>
         </Container>
     </>)
 }
